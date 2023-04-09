87362d43294fb146e68bb3c537c07288207fa136
Alexander Herrera <chander.zap@gmail.com>
2023-04-09 03:41:12 -0500

    [fix] - disabling unnused for user service tests

--- src/components/users/__tests__/user.service.spec.ts
+++ src/components/users/__tests__/user.service.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { authenticateUser, createNewUser } from '../service'
 import { insertUser, queryUser } from '../queries'
 import { UniqueConstraintError } from 'sequelize'
